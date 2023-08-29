af8a2c98b43c302f3edcbb7e5180bd97bb02f12d
laxminara1996 <laxminara1996@gmail.com>
2023-08-29 19:54:13 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedmoduleModule } from './sharedmodule/sharedmodule.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,SharedmoduleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
